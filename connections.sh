c20e30913efb4b05bb41fb9efedd6f5746d7ace4
Dean Morin <morin.dean@gmail.com>
2012-03-04 18:19:25 -0800

    okay, so scope isn't an issue per se, but there are other bashisms that can sting you

--- connections.sh
+++ connections.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+prev=-1
+
 while [ 1 ]; do
     count=$(netstat -aon | grep 32000 | wc -l)
     if [ $count -eq 0 ]; then
         echo "Server is not running." 
-        sleep 5
-    fi
-    count=$(($count - 1))
-    if [ $count -ne $prev ]; then
-        echo Connected clients: $count
-        prev=$count
+        # wait for server to come up
+        while [ ! $(netstat -aon | grep 32000 | wc -l) ]; do
+            sleep 1
+        done
+    else
+        # don't count the listening socket
+        count=$(($count - 1))
+        if [ "$count" -ne "$prev" ]; then
+            echo Connected clients: $count
+            prev=$count
+        fi
     fi
 done
