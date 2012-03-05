336fea42ad1f5b11ac67142e6c5b10c13dae1970
Dean Morin <morin.dean@gmail.com>
2012-03-04 17:57:26 -0800

    does bash have a concept of scope? let's find out

--- connections.sh
+++ connections.sh
@@ -2,8 +2,13 @@
 
 while [ 1 ]; do
     count=$(netstat -aon | grep 32000 | wc -l)
+    if [ $count -eq 0 ]; then
+        echo "Server is not running." 
+        sleep 5
+    fi
     count=$(($count - 1))
-    echo Connected clients: $count
-
-    sleep 1
+    if [ $count -ne $prev ]; then
+        echo Connected clients: $count
+        prev=$count
+    fi
 done
